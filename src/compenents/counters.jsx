cc0bfc3a4fdffb7d27f4416d046d283cb9857d7d
unknown <jgmrathnasiri@gmail.com>
2020-03-19 01:25:37 +0800

    completion of simple application

--- src/compenents/counters.jsx
+++ src/compenents/counters.jsx
@@ -2,26 +2,25 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = { 
-        counters : [
-            {id: 1, value: 0},
-            {id: 2, value: 2},
-            {id: 3, value: 3},
-            {id: 4, value: 4}
-        ]
-     }
+    
     render() { 
-        return ( <div>
-            {this.state.counters.map( c => 
-            <Counter key={c.id} value={c.value} onDelete={this.handleDelete}>
-            </Counter>
+        //Object Destructuring for a clean code
+        const {onReset, counters, onIncrement, onDelete} = this.props;
+        return ( 
+        <div>
+            <button className="btn btn-primary btn-sm m-2" onClick={onReset}>Reset</button>
+            {counters.map( c => 
+                <Counter 
+                    key={c.id} 
+                    counter={c}
+                    onReset={onReset} 
+                    onIncrement={onIncrement}
+                    onDelete={onDelete} 
+                >
+                </Counter>
             )}
         </div>);
     }
-
-    handleDelete = () => {
-        console.log('DELETE Called');
-    };
 }
  
 export default Counters;
